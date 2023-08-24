8603e505b54dac7ad98e9c7a27208f149bcf5535
crimeiscool <139124034+crimeiscool@users.noreply.github.com>
2023-08-24 21:14:18 +0200

    test.lua

---
+++ test.lua
@@ -0,0 +1,32 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/crimeiscool/Orion/main/test')))()
+
+local Window = OrionLib:MakeWindow({Name = "Counter Blox / Darklife Script Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+local Tab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Section"
+})
+
+Tab:AddSlider({
+	Name = "Walkspeed",
+	Min = 16,
+	Max = 450,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "WS",
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end    
+})
+
+
+
+
+
